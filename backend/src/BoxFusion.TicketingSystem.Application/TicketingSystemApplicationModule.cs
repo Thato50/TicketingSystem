c6c285028505c0f826fbedeccbadabcd8669bf67
Thato <thatomphahlele57@gmail.com>
2026-04-11 12:10:38 +0200

    Initial commit: Shesha Ticketing System setup

---
+++ backend/src/BoxFusion.TicketingSystem.Application/TicketingSystemApplicationModule.cs
@@ -0,0 +1,90 @@
+using System.Reflection;
+using System.Threading.Tasks;
+using Abp.AspNetCore;
+using Abp.AspNetCore.Configuration;
+using Abp.AutoMapper;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+using Intent.RoslynWeaver.Attributes;
+using BoxFusion.TicketingSystem.Domain;
+using Shesha;
+using Shesha.Modules;
+using Shesha.Startup;
+using Shesha.Web.FormsDesigner;
+
+[assembly: IntentTemplate("Boxfusion.Modules.Application.Services.AppService", Version = "1.0")]
+[assembly: DefaultIntentManaged(Mode.Fully)]
+
+namespace BoxFusion.TicketingSystem.Application
+{
+    [IntentManaged(Mode.Ignore)]
+    /// <summary>
+    /// TicketingSystem Module
+    /// </summary>
+    [DependsOn(
+        typeof(TicketingSystemModule),
+        typeof(SheshaCoreModule),
+        typeof(AbpAspNetCoreModule)
+    )]
+    public class TicketingSystemApplicationModule : SheshaSubModule<TicketingSystemModule>
+    {
+        public override async Task<bool> InitializeConfigurationAsync()
+        {
+            // Import any configuration embeded as resources in this assembly on application start-up.
+            return await ImportConfigurationAsync();
+        }
+
+        /// inheritedDoc
+        public override void Initialize()
+        {
+            var thisAssembly = Assembly.GetExecutingAssembly();
+            IocManager.RegisterAssemblyByConvention(thisAssembly);
+
+            Configuration.Modules.AbpAutoMapper().Configurators.Add(
+                // Scan the assembly for classes which inherit from AutoMapper.Profile
+                cfg => cfg.AddMaps(thisAssembly)
+            );
+        }
+
+        /// inheritedDoc
+        public override void PreInitialize()
+        {
+            base.PreInitialize();
+
+            Configuration.Modules.AbpAspNetCore()
+                .CreateControllersForAppServices(
+                    typeof(SheshaCoreModule).GetAssembly()
+                );
+
+            Configuration.Modules.AbpAspNetCore()
+                 .CreateControllersForAppServices(
+                     typeof(SheshaApplicationModule).GetAssembly()
+                 );
+
+            Configuration.Modules.AbpAspNetCore()
+                 .CreateControllersForAppServices(
+                     typeof(SheshaFormsDesignerModule).GetAssembly()
+                 );
+
+            Configuration.Modules.AbpAspNetCore()
+                 .CreateControllersForAppServices(
+                     typeof(SheshaFrameworkModule).GetAssembly()
+                 );
+
+            Configuration.Modules.AbpAspNetCore().CreateControllersForAppServices(
+               typeof(TicketingSystemApplicationModule).Assembly,
+               moduleName: "TicketingSystem",
+                useConventionalHttpVerbs: true);
+
+            Configuration.Modules.AbpAspNetCore()
+                 .CreateControllersForAppServices(
+                     typeof(TicketingSystemModule).GetAssembly()
+                 );
+
+            Configuration.Modules.AbpAspNetCore()
+                 .CreateControllersForAppServices(
+                     typeof(TicketingSystemApplicationModule).GetAssembly()
+                 );
+        }
+    }
+}
---
+++ backend/src/BoxFusion.TicketingSystem.Web.Core/TicketingSystemWebCoreModule.cs
@@ -0,0 +1,83 @@
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using BoxFusion.TicketingSystem.Application;
+using BoxFusion.TicketingSystem.Domain;
+using Shesha;
+using Shesha.Authentication.JwtBearer;
+using Shesha.Configuration;
+using Shesha.Configuration.Startup;
+using Shesha.Elmah;
+using Shesha.Import;
+using Shesha.Sms.Clickatell;
+using Shesha.Web.FormsDesigner;
+using System;
+using System.Text;
+
+namespace BoxFusion.TicketingSystem
+{
+    /// <summary>
+    /// ReSharper disable once InconsistentNaming
+    /// </summary>
+    [DependsOn(
+        // Adding all the TicketingSystem Modules
+        typeof(SheshaFrameworkModule),
+        typeof(SheshaApplicationModule),
+        typeof(SheshaFormsDesignerModule),
+        typeof(SheshaElmahModule),
+        typeof(SheshaImportModule),
+        typeof(SheshaClickatellModule),
+        typeof(TicketingSystemModule),
+        typeof(TicketingSystemApplicationModule)
+	 )]
+    public class TicketingSystemWebCoreModule : AbpModule
+    {
+        private readonly IConfigurationRoot _appConfiguration;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="env"></param>
+        public TicketingSystemWebCoreModule(IWebHostEnvironment env)
+        {
+            _appConfiguration = env.GetAppConfiguration();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void PreInitialize()
+        {
+            var config = Configuration.Modules.ShaNHibernate();
+            
+            config.UseDbms(c => c.GetDbmsType(), c => c.GetDefaultConnectionString());
+
+            //config.UseMsSql();
+            //config.UsePostgreSql();
+
+            ConfigureTokenAuth();
+        }
+
+        private void ConfigureTokenAuth()
+        {
+            IocManager.Register<TokenAuthConfiguration>();
+            var tokenAuthConfig = IocManager.Resolve<TokenAuthConfiguration>();
+
+            tokenAuthConfig.SecurityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_appConfiguration["Authentication:JwtBearer:SecurityKey"]));
+            tokenAuthConfig.Issuer = _appConfiguration["Authentication:JwtBearer:Issuer"];
+            tokenAuthConfig.Audience = _appConfiguration["Authentication:JwtBearer:Audience"];
+            tokenAuthConfig.SigningCredentials = new SigningCredentials(tokenAuthConfig.SecurityKey, SecurityAlgorithms.HmacSha256);
+            tokenAuthConfig.Expiration = TimeSpan.FromDays(5);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(typeof(TicketingSystemWebCoreModule).GetAssembly());
+        }
+    }
+}
