e6f59ea24858defe8dc10505af53f93abae7832b
Thato <thatomphahlele57@gmail.com>
2026-04-21 09:34:30 +0200

    fixed the script for the login

--- adminportal/src/app/page.tsx
+++ adminportal/src/app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  redirect('/dynamic/BoxFusion.TicketingSystem/Landing-page');
+  redirect('/no-auth/BoxFusion.TicketingSystem/Landing-page');
 }
